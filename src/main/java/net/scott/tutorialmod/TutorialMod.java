12a16463e757d4f9be95aaececf293ab013251f9
Scott ergle <scottergle06@gmail.com>
2023-01-02 21:51:36 -0500

    Initial commit

---
+++ src/main/java/net/scott/tutorialmod/TutorialMod.java
@@ -0,0 +1,15 @@
+package net.scott.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//very important message
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
